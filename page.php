d6bf9d29ba69bd3c368ae04878999b5a7cfb2e28
Md Ibrahim Rana <rana.infonet@gmail.com>
2019-11-19 20:52:42 +0600

    show the list of child page under parent page

--- page.php
+++ page.php
@@ -1,5 +1,39 @@
-#WP List Pages
+# WP List Pages
 
+** Display all the pages **
 <?php
-wp_list_pages(); //Display all the pages with a title
+wp_list_pages(); //Display all the pages with a title at top 'Pages'
+?>
 
+
+** Show Child pages of Parant page **
+<?php
+  $parentPageId = wp_get_post_parent_id( get_the_ID() );
+
+  $pageHasChild = get_pages(array(
+    'child_of'  => get_the_id
+  ));
+
+  if( $parentPageId or $pageHasChild) {
+?>
+
+    <div class="page-links">
+      <h2 class="page-links-title"><a href="<?php echo get_permalink($parentPageId);?>"><?php echo get_the_title($parentPageId);?></a></h2>
+      <ul class="min-list">
+        <?php
+        if( $parentPageId ) {
+          $childPageId = $parentPageId;
+        } else {
+          $childPageId = get_the_ID;
+        }
+
+        wp_list_pages(array(
+          'title_li'  => NULL,
+          'child_of'  => $childPageId,
+          'sort_column'	=> 'menu_order' //has options under Page Attributes
+        ));
+        ?>
+      </ul>
+    </div>
+
+<?php } ?>

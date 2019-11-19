82dd1e1eeff020568436c333b6b24b400a9d209d
Md Ibrahim Rana <rana.infonet@gmail.com>
2019-11-19 21:09:04 +0600

    edit few lines of code and add comment.

--- page.php
+++ page.php
@@ -8,10 +8,12 @@ wp_list_pages(); //Display all the pages with a title at top 'Pages'
 
 ** Show Child pages of Parant page **
 <?php
+  //get the page parent ID
   $parentPageId = wp_get_post_parent_id( get_the_ID() );
-
+  
+  //checking if the page has a child page or not
   $pageHasChild = get_pages(array(
-    'child_of'  => get_the_id
+    'child_of'  => get_the_ID()
   ));
 
   if( $parentPageId or $pageHasChild) {
@@ -21,10 +23,11 @@ wp_list_pages(); //Display all the pages with a title at top 'Pages'
       <h2 class="page-links-title"><a href="<?php echo get_permalink($parentPageId);?>"><?php echo get_the_title($parentPageId);?></a></h2>
       <ul class="min-list">
         <?php
+        //get parent page ID if it's a parent OR get the child page ID.
         if( $parentPageId ) {
           $childPageId = $parentPageId;
         } else {
-          $childPageId = get_the_ID;
+          $childPageId = get_the_ID();
         }
 
         wp_list_pages(array(
